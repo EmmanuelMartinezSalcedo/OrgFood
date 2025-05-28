9e98adc464317a60fe0045cf9361f5185412f348
Emmanuel <emmanuel.martinez@ucsp.edu.pe>
2025-05-28 10:59:34 -0500

    Producers section

--- src/app/components/home/featured-producers/featured-producers.component.ts
+++ src/app/components/home/featured-producers/featured-producers.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FeaturedProducerCardComponent } from '../featured-producer-card/featured-producer-card.component';
 import { FeaturedProducer } from '../../../domain/interfaces/featured-producer.interface';
 import { ResponsiveService } from '../../../services/responsive.service';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-featured-producers',
-  imports: [FeaturedProducerCardComponent, NzGridModule],
+  imports: [FeaturedProducerCardComponent, NzGridModule, NzTypographyModule],
   templateUrl: './featured-producers.component.html',
   styleUrl: './featured-producers.component.less',
 })
-export class FeaturedProducersComponent implements OnInit {
+export class FeaturedProducersComponent implements OnInit, OnDestroy {
   tempProducers: FeaturedProducer[] = [];
   private subscription: Subscription = new Subscription();
+  span: number = 6;
+
   constructor(private responsiveService: ResponsiveService) {}
   ngOnInit(): void {
     this.tempProducers = [
@@ -66,6 +69,23 @@ export class FeaturedProducersComponent implements OnInit {
         name: 'Leven Rose',
       },
     ];
-    this.subscription = this.responsiveService.screenSize$.subscribe(() => {});
+
+    this.subscription = this.responsiveService.screenSize$.subscribe(() => {
+      if (this.responsiveService.isSmallScreen()) {
+        this.span = 24;
+      } else if (this.responsiveService.isMediumScreen()) {
+        this.span = 12;
+      } else if (this.responsiveService.isLargeScreen()) {
+        this.span = 8;
+      } else {
+        this.span = 6;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
